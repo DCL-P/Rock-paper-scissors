0659f8ddea15742ad6ff742cbdb44d8db8adc0c9
Buzip <164242635+DCL-P@users.noreply.github.com>
2024-03-20 22:01:42 +0100

    converted returns to english
    made this program in dutch first, converted it too english, seems like i forgot to convert the returns to english.

--- functionsRPS.py
+++ functionsRPS.py
@@ -1,25 +1,25 @@
 
 def PossibleGames(invoer_robot:str, invoer_player:str) -> str:
     if invoer_robot == 'rock' and invoer_player == 'paper':
-        return 'je hebt gewonnen!'
+        return 'u won!'
     elif invoer_robot == 'paper' and invoer_player == 'rock':
-        return 'je hebt verloren!'
+        return 'u lost! :('
     elif invoer_robot == 'paper' and invoer_player == 'scissors':
-        return 'je hebt gewonnen!'
+        return 'u won!'
     elif invoer_robot == 'scissors' and invoer_player == 'paper':
-        return 'je hebt verloren!'
+        return 'u lost! :('
     elif invoer_robot == 'scissors' and invoer_player == 'rock':
-        return 'je hebt gewonnen!'
+        return 'u won!'
     elif invoer_robot == 'rock' and invoer_player == 'scissors':
-        return 'je hebt verloren!'
+        return 'u lost! :('
     elif invoer_robot == 'rock' and invoer_player == 'paper':
-        return 'je hebt gewonnen!'
+        return 'u won!'
     elif invoer_robot == 'paper' and invoer_player == 'rock':
-        return 'je hebt verloren!'
+        return 'u lost! :('
     elif invoer_robot == 'paper' and invoer_player == 'paper':
-        return 'je hebt gelijk gespeelt!'
+        return "it's a draw! :O"
     elif invoer_robot == 'rock' and invoer_player == 'rock':
-        return 'je hebt gelijk gespeelt!'
+        return "it's a draw! :O"
     elif invoer_robot == 'scissors' and invoer_player == 'scissors':
-        return 'je hebt gelijk gespeelt!'
+        return "it's a draw! :O"
 
